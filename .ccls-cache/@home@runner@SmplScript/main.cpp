9dade2e97669d8decb19fd99052fee0c59d65f11
Stephen Steyaert <65798269+CubesandSLEIGHTS@users.noreply.github.com>
2022-02-17 19:15:05 +0000

    Add line editing and history with linenoise

--- .ccls-cache/@home@runner@SmplScript/main.cpp
+++ .ccls-cache/@home@runner@SmplScript/main.cpp
@@ -1,11 +1,16 @@
 #include "step0.h"
+#include "linenoise.hpp"
 
 int main() {
+  const auto history_path = "history.txt";
+  linenoise::LoadHistory(history_path);
   std::string input;
   for (;;){
-    std::cout<<"user> ";
-    if (!std::getline(std::cin, input)) break;
+    auto quit = linenoise::Readline("user> ", input);
+    if (quit) break;
+    linenoise::AddHistory(input.c_str());
     std::cout << rep(input) << std::endl;
   }
+  linenoise::SaveHistory(history_path);
   return 0;
 }
